5b970ea20e8c40fe05f687dcb559600b55139911
shweta-0909 <136495218+shweta-0909@users.noreply.github.com>
2023-06-14 19:38:22 +0530

    Add files via upload

---
+++ module1/day1/level1/ques_4.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+  int a, b, res;
+  char c;
+  printf ("Enter any one operator +, -, *, / n");
+  scanf("%c", &c);
+  printf ("n Enter two numbers n");
+  scanf ("n %d n %d",&a, &b);
+
+  if (c=='+')
+  {   
+    res=a+b;
+    printf("n The sum is %d",res);
+  }
+
+  else if(c== '-')
+  {
+    res=a-b;
+    printf("n The difference is %d",res);
+  }
+
+  else if(c== '*')
+  {
+    res=a*b;
+    printf("n The product is %d",res);
+  }
+
+  else if(c=='/')
+  {
+    res=a/b;
+    printf("n The quotient is %d",res);
+  }
+
+  else
+  {
+    printf ("n Invalid entry");
+  }
+
+  return 0;
+}
+
+
+
