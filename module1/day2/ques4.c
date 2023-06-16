34937cfc524f8fc0eff173e911b188d95db2e9ce
shweta-0909 <136495218+shweta-0909@users.noreply.github.com>
2023-06-16 19:40:33 +0530

    Update ques4.c

--- module1/day2/ques4.c
+++ module1/day2/ques4.c
@@ -1,20 +1,54 @@
+//total number of bits set
+
 #include <stdio.h>
 #include <string.h>
  
-int countSetBits(unsigned int n)
+#include <stdio.h>
+
+int countSetBits(int num)
 {
-  unsigned int c;
-  for (c = 0; n; n >>= 1)
-  {
-    c += n & 1;
-  }
-  return c;
+    int count = 0;
+    
+    while (num != 0)
+    {
+        count += num & 1;  // Check the rightmost bit of num
+        
+        num >>= 1;   // Right shift num by 1 bit
+    }
+    
+    return count;
 }
- 
-int main(void)
+
+int getTotalSetBits(int arr[], int size)
 {
-  unsigned int n;
-  printf("Enter a positive integer: ");
-  scanf("%u", &n);
-  printf("%d\n", countSetBits(n));
+    int totalBits = 0;
+    
+    for (int i = 0; i < size; i++)
+    {
+        totalBits += countSetBits(arr[i]);
+    }
+    
+    return totalBits;
+}
+
+int main()
+{
+    int size;
+    
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    
+    int arr[size];
+    
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    int totalSetBits = getTotalSetBits(arr, size);
+    
+    printf("Total number of set bits in the array: %d\n", totalSetBits);
+    
+    return 0;
 }
