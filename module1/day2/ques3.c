7dfa854529d675a2e06d3e4e83b9519dc391f9fa
shweta-0909 <136495218+shweta-0909@users.noreply.github.com>
2023-06-16 19:38:52 +0530

    Update ques3.c

--- module1/day2/ques3.c
+++ module1/day2/ques3.c
@@ -1,20 +1,37 @@
+// sum of alternate arrays
+
 #include <stdio.h>
 
-
-    void main()
+int sumAlternateElements(int arr[], int size)
+{
+    int sum = 0;
+    
+    for (int i = 0; i < size; i += 2)
     {
- 
-        int array[5];
-        int i;
-        int sum=0;
-        printf("enter the element of an array \n");
-        for (i = 0; i < 5 ; i++)
-            scanf("%d", &array[i]);
- 
-        printf("Alternate elements of a given array \n");
-        for (i = 0; i < 5; i += 2)
-            printf( "%d\n", array[i]) ;
-
-        sum = sum + array[i];
-        printf("Sum of the array = %d\n",sum);
+        sum += arr[i];
     }
+    
+    return sum;
+}
+
+int main()
+{
+    int size;
+    
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    
+    int arr[size];
+    
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    int sum = sumAlternateElements(arr, size);
+    
+    printf("Sum of alternate elements: %d\n", sum);
+    
+    return 0;
+}
