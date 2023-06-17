1b55381ea88a521cb812d8408c8090826eccff6c
shweta-0909 <136495218+shweta-0909@users.noreply.github.com>
2023-06-17 21:44:43 +0530

    Add files via upload

---
+++ module1/day4/ques1.c
@@ -0,0 +1,26 @@
+// sum and average of 1D array
+
+ #include <stdio.h>
+
+int main() {
+    int size, i;
+    float sum = 0, average;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int array[size];
+
+    printf("Enter the elements of the array:\n");
+    for (i = 0; i < size; i++) {
+        scanf("%d", &array[i]);
+        sum += array[i];
+    }
+
+    average = sum / size;
+
+    printf("Sum of the array elements: %.2f\n", sum);
+    printf("Average of the array elements: %.2f\n", average);
+
+    return 0;
+}
